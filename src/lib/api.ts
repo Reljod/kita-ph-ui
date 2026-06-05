48a0df43f405dded708ee74435b4101493cc2408
Jod <oretareljod@gmail.com>
2026-06-05 20:23:00 +0800

    chore: update port to use 8000

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 export const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080',
+    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
     headers: {
         'Content-Type': 'application/json',
     },
